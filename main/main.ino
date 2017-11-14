9d470de5f475f4a8f26f5c6d88f19e4cd5ca08d9
PattarapH <pataraporn.han@gmail.com>
2017-11-15 00:54:42 +0700

    complete snooze with one meal and choose amount of tablet

--- main/main.ino
+++ main/main.ino
@@ -1,3 +1,4 @@
+
 #include <Servo.h>
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
@@ -9,9 +10,11 @@ short sec = 0;
 int count_1 = 0;
 int count_2 = 0;
 
-short eat_hr = 22;
-short eat_mins = 1;
+short morn_eat_hr = 22;
+short morn_eat_mins = 1;
 short mins_snooze = -1;
+short cnt_tablet = 2;
+short cnt_meals = 1;
 
 const int SWITCH_EAT = PIN_PB1;
 const int SWITCH_SNOOZE = PIN_PB0;
@@ -23,6 +26,7 @@ const int LED_GREEN = PIN_PC1;
 
 bool firstEat = true;
 bool Eaten = false;
+bool press_for_snooze = false;
 
 Servo servo;
 
@@ -40,20 +44,14 @@ void setup()
   lcd.setCursor(0,3);
   lcd.print("sec");*/
   count_1 = millis();
-//  pinMode(PIN_PD6,OUTPUT);
-//  digitalWrite(PIN_PD6, HIGH);
-//  servo.attach(PIN_PC1);
   pinMode(SWITCH_EAT,INPUT_PULLUP);
   pinMode(SWITCH_SNOOZE,INPUT_PULLUP);
   pinMode(BUZZER,OUTPUT);
   pinMode(LED_RED,OUTPUT);
   pinMode(LED_GREEN,OUTPUT);
-//  pinMode(SERVO,OUTPUT);
   digitalWrite(BUZZER,HIGH);
   servo.attach(PIN_PB2);
-  servo.writeMicroseconds(1500);
   servo.write(0); 
-  delay(1000);
 }
 
 void updateTime() {
@@ -77,47 +75,53 @@ void updateTime() {
     count_1 = count_2;
   }
 }
-
+void eat()
+{
+  int i=1;
+  digitalWrite(LED_GREEN,LOW);
+  digitalWrite(BUZZER,HIGH);
+  for(;i<=cnt_tablet;i++){        
+    servo.write(200); 
+    delay(1000);      
+    servo.write(0); 
+    delay(1000);
+  }
+  cnt_meals++;   
+}
+void snooze(){
+  if(press_for_snooze && mins-mins_snooze >= 5){
+    digitalWrite(BUZZER,LOW);
+    if (digitalRead(SWITCH_EAT) == LOW) {
+       eat();
+       mins_snooze = -1;   
+       press_for_snooze = false; 
+    }
+   }
+}
 void loop()
 {
   updateTime();
-  if(hr == eat_hr && mins == eat_mins && !Eaten) {
+  //morning
+  if(hr == morn_eat_hr && mins == morn_eat_mins && cnt_meals == 1) {
     if (firstEat) {
       firstEat = false;
       digitalWrite(LED_GREEN,HIGH);
-//      delay(500);
       digitalWrite(BUZZER, LOW);
-//      delay(500);
     }   
     //eat
     if (digitalRead(SWITCH_EAT) == LOW) {
-      digitalWrite(LED_GREEN,LOW);
-      digitalWrite(BUZZER,HIGH);        
-       servo.write(200); 
-       delay(1000);      
-       servo.write(0); 
-       delay(1000);
-       Eaten = true;    
+       eat();
     }
     //snooze
     if(digitalRead(SWITCH_SNOOZE) == LOW){
       digitalWrite(BUZZER,HIGH);
       mins_snooze = mins;
+      press_for_snooze = true;
       }
-    }
-   if(mins - mins_snooze >= 5){
-      mins_snooze = 0;
-      digitalWrite(BUZZER,LOW);
-      if (digitalRead(SWITCH_EAT) == LOW) {
-        digitalWrite(LED_GREEN,LOW);
-        digitalWrite(BUZZER,HIGH);        
-        servo.write(200); 
-        delay(1000);      
-        servo.write(0); 
-        delay(1000);
-        Eaten = true;    
-      }
+   }
+   snooze();
 }
-}
+
+
 
 
