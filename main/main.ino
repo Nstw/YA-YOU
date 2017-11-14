77bd03c3ec704e6d17a4c6a3455e9fdf56890b4c
PattarapH <pataraporn.han@gmail.com>
2017-11-14 21:54:30 +0700

    snooze

--- main/main.ino
+++ main/main.ino
@@ -11,17 +11,21 @@ int count_2 = 0;
 
 short eat_hr = 22;
 short eat_mins = 1;
+short mins_snooze = -1;
 
-const int SWITCH_EAT = PIN_PB0;
-const int SWITCH_SNOOZE = PIN_PB1;
+const int SWITCH_EAT = PIN_PB1;
+const int SWITCH_SNOOZE = PIN_PB0;
 //const int SERVO = PIN_PB2;
 const int BUZZER = PIN_PB3;
 const int LED_RED = PIN_PC0;
 const int LED_GREEN = PIN_PC1;
+//const int SERVO = PIN_PB2;
 
-
+bool firstEat = true;
+bool Eaten = false;
 
 Servo servo;
+
 void setup()
 {
   lcd.init();                      // initialize the lcd 
@@ -44,12 +48,15 @@ void setup()
   pinMode(BUZZER,OUTPUT);
   pinMode(LED_RED,OUTPUT);
   pinMode(LED_GREEN,OUTPUT);
+//  pinMode(SERVO,OUTPUT);
   digitalWrite(BUZZER,HIGH);
   servo.attach(PIN_PB2);
+  servo.writeMicroseconds(1500);
+  servo.write(0); 
+  delay(1000);
 }
 
-void loop()
-{
+void updateTime() {
   count_2 = millis();
   if(mins >= 60){
     mins = 0;
@@ -69,10 +76,48 @@ void loop()
     lcd.print(sec);
     count_1 = count_2;
   }
-  if(hr == eat_hr && mins == eat_mins){
-    digitalWrite(LED_GREEN,HIGH);
-    delay(500);
-    tone(BUZZER,1000);
-
-  }
 }
+
+void loop()
+{
+  updateTime();
+  if(hr == eat_hr && mins == eat_mins && !Eaten) {
+    if (firstEat) {
+      firstEat = false;
+      digitalWrite(LED_GREEN,HIGH);
+//      delay(500);
+      digitalWrite(BUZZER, LOW);
+//      delay(500);
+    }   
+    //eat
+    if (digitalRead(SWITCH_EAT) == LOW) {
+      digitalWrite(LED_GREEN,LOW);
+      digitalWrite(BUZZER,HIGH);        
+       servo.write(200); 
+       delay(1000);      
+       servo.write(0); 
+       delay(1000);
+       Eaten = true;    
+    }
+    //snooze
+    if(digitalRead(SWITCH_SNOOZE) == LOW){
+      digitalWrite(BUZZER,HIGH);
+      mins_snooze = mins;
+      }
+    }
+   if(mins - mins_snooze >= 5){
+      mins_snooze = 0;
+      digitalWrite(BUZZER,LOW);
+      if (digitalRead(SWITCH_EAT) == LOW) {
+        digitalWrite(LED_GREEN,LOW);
+        digitalWrite(BUZZER,HIGH);        
+        servo.write(200); 
+        delay(1000);      
+        servo.write(0); 
+        delay(1000);
+        Eaten = true;    
+      }
+}
+}
+
+
